981d59e61c10f4fbbc5722fdcbe635fd6bcd807f
poorva-r <poorva.ramnani@gmail.com>
2025-01-19 18:37:01 +0400

    button

--- src/components/FinishedScreen.js
+++ src/components/FinishedScreen.js
@@ -1,16 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-
-const FinishedScreen = ({score, startGame}) => {
-    return ( 
-        <div>
-            <div className='text-3xl font-bold'>Congratulations</div>
-            <div className='text-3xl font-bold mt-4'>Your Score is {score} </div>
-            <button className='bg-purple-950 p-4 rounded-xl shadow-xl text-white mt-12 text-3xl' onClick={startGame}>
-                Restart Game
-            </button>
+const FinishedScreen = ({ score, startGame }) => {
+  return (
+    <div>
+      <div className="text-3xl font-bold">Congratulations</div>
+      <div className="text-3xl font-bold mt-4 mb-8">Your Score is {score} </div>
+      <button
+        class="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-black backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-blue-700/50 border border-white/20"
+        onClick={startGame}
+      >
+        <span class="text-lg">Restart Game</span>
+        <div class="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
+          <div class="relative h-full w-10 bg-white/30"></div>
         </div>
-     );
-}
- 
+      </button>
+    </div>
+  );
+};
+
 export default FinishedScreen;
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-function StartScreen({startGame}) {
-    return ( 
-
-<div className='text-4xl text-black font-bold'>
-    <div>Memory Matching Game</div>
-    <button className='bg-purple-950 p-4 rounded-xl shadow-xl text-white mt-20' onClick={startGame}>
-        Start Game
-    </button>
-</div>
-
-
-     );
+function StartScreen({ startGame }) {
+  return (
+    <div className="text-4xl text-black font-bold">
+      <div className="mb-10">Memory Matching Game</div>
+      <button class="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-black backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-blue-700/50 border border-white/20"
+      onClick={startGame}>
+        <span class="text-lg">Start Game</span>
+        <div class="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
+          <div class="relative h-full w-10 bg-white/30"></div>
+        </div>
+      </button>
+    </div>
+  );
 }
 
 export default StartScreen;
-
