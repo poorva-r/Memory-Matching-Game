bfa37e0366e8e8b3b913124fe91d0669d5edc1eb
poorva-r <poorva.ramnani@gmail.com>
2025-01-24 22:04:01 +0400

    ui changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const Card = ({content, flipped, matched, onClick}) => {
+const Card = ({ content, flipped, matched, onClick }) => {
+  return (
+    <div
+      className={`border-4 border-white text-white shadow-lg flex text-center items-center justify-center rounded-lg text-3xl md:text-5xl w-14 h-16 md:w-28 md:h-36 lg:w-32 lg:h-38 ${
+        flipped || matched ? "bg-pink-350" : "bg-gray-700"
+      }`}
+      onClick={onClick}
+    >
+      {flipped || matched ? content : "?"}
+    </div>
+  );
+};
 
-
-    return ( 
-        <div className={`border shadow-lg flex text-center items-center justify-center rounded-lg text-3xl md:text-5xl w-14 h-16 md:w-28 md:h-36 lg:w-32 lg:h-38 ${flipped || matched ? 'bg-pink-300' : 'bg-purple-300'}`}
-        onClick={onClick}>
-            {flipped || matched ? content : "?"}
-        </div>
-
-     );
-}
- 
 export default Card;
