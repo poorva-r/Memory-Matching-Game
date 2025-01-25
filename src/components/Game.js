d27f4cb754d6ee534efe5022b8e369a0963a91dc
poorva-r <poorva.ramnani@gmail.com>
2025-01-25 14:21:42 +0400

    formatting

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,80 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import { cardPairs } from '../data/cards';
-import Card from './Card';
+import React, { useState, useEffect } from "react";
+import { cardPairs } from "../data/cards";
+import Card from "./Card";
 
+const Game = ({ finishGame, setScore, score }) => {
+  // make a shuffled copy of the cards
+  const shuffledCards = [...cardPairs].sort(() => Math.random() - 0.5);
+  const [cards, setcards] = useState(shuffledCards);
+  const [flippedCards, setflippedCards] = useState([]); // max 2 cards
+  const [matchedCards, setmatchedCards] = useState([]);
 
-const Game = ({finishGame, setScore, score}) => {
-    // make a shuffled copy of the cards
-    const shuffledCards = [...cardPairs].sort(() => Math.random()-0.5);
-    const [cards, setcards] = useState(shuffledCards);
-    const [flippedCards, setflippedCards] = useState([]);  // max 2 cards
-    const [matchedCards, setmatchedCards] = useState([]);
+  const handleCardClick = (id) => {
+    // prevent invalid clicks
+    if (matchedCards.includes(id) || flippedCards.includes(id)) return;
 
-    const handleCardClick = (id) => {
+    // change state for flipped ones
+    const updatedFlipped = [...flippedCards, id];
+    setflippedCards(updatedFlipped);
 
-        // prevent invalid clicks
-        if (matchedCards.includes(id) || flippedCards.includes(id)) return;
+    // check for match if 2 are flipped
+    if (updatedFlipped.length === 2) {
+      checkMatch(updatedFlipped);
+    }
+  };
 
-        // change state for flipped ones
-        const updatedFlipped = [...flippedCards, id];
-        setflippedCards(updatedFlipped);
+  const checkMatch = (flipped) => {
+    // check if content matches of the 2 cards in flipped
+    // if match add to matchedCards else keep a delay and remove from flipped
 
-        // check for match if 2 are flipped
-        if (updatedFlipped.length === 2)
-        {
-            checkMatch(updatedFlipped);
-        }
-      
-    };
+    const [firstId, secondId] = flipped;
 
-    const checkMatch = (flipped) => {
+    const firstCard = cards.find((card) => card.id === firstId);
+    const secondCard = cards.find((card) => card.id === secondId);
 
-        // check if content matches of the 2 cards in flipped
-        // if match add to matchedCards else keep a delay and remove from flipped
+    if (firstCard.content === secondCard.content) {
+      setmatchedCards((prev) => [...prev, firstId, secondId]);
+      setflippedCards([]);
+      setScore(score + 1);
+    } else {
+      setTimeout(() => {
+        setflippedCards([]);
+      }, 800);
+    }
+  };
 
-        const [firstId, secondId] = flipped;
+  useEffect(() => {
+    if (matchedCards.length === cards.length) {
+      finishGame();
+    }
+  }, [matchedCards]);
 
-        const firstCard = cards.find(card => card.id === firstId);
-        const secondCard = cards.find(card => card.id === secondId);
+  return (
+    <div>
+      <div className="grid grid-cols-4 gap-5 md:gap-8 p-5 md:p-10">
+        {cards.map((card) => (
+          <Card
+            key={card.id}
+            content={card.content}
+            flipped={flippedCards.includes(card.id)}
+            matched={matchedCards.includes(card.id)}
+            onClick={() => handleCardClick(card.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-        if (firstCard.content === secondCard.content)
-        {
-            setmatchedCards(prev => [...prev, firstId, secondId]);
-            setflippedCards([]);
-            setScore(score + 1);
-        }
-        else
-        {
-            setTimeout(() => {
-                setflippedCards([]);
-            }, 800);
-        }
-    };
-
-    useEffect(() => {
-        if (matchedCards.length === cards.length)
-        {
-            finishGame();
-        }
-        
-    }, [matchedCards]);
-
-
-    return ( 
-        <div>
-            <div className='grid grid-cols-4 gap-5 md:gap-8 p-5 md:p-10'>
-                {cards.map((card) => (
-                    <Card 
-                    key = {card.id}
-                    content = {card.content}
-                    flipped = {flippedCards.includes(card.id)}
-                    matched = {matchedCards.includes(card.id)}
-                    onClick = {() => handleCardClick(card.id)}
-                    />
-                ))}
-            </div>
-        </div>
-     );
-}
- 
 export default Game;
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -4,8 +4,10 @@ function StartScreen({ startGame }) {
   return (
     <div className="text-4xl md:text-5xl font-kalam text-black font-bold">
       <div className="mb-10">Memory Matching Game</div>
-      <button className="cursor-custom group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gray-700 backdrop-blur-lg px-6 py-2 text-base font-semibold border-2 border-black text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-blue-700/50"
-      onClick={startGame}>
+      <button
+        className="cursor-custom group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gray-700 backdrop-blur-lg px-6 py-2 text-base font-semibold border-2 border-black text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-blue-700/50"
+        onClick={startGame}
+      >
         <span className="text-2xl">Start Game</span>
         <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
           <div className="relative h-full w-10 bg-white/30"></div>
